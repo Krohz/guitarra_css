7577ff001aff0c6675041ed49c5f5ada44a558d3
Kros <cristian@alvias.com>
2022-01-22 11:58:54 -0500

    header end

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const icon = document.getElementById("icon");
+
 icon.onclick = function (){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
